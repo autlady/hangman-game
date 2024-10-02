79bd8cac141f7a6e459fe4f317458e7e872fb56d
Julia Grigoreva <julia_panina@mail.ru>
2024-10-02 15:13:27 +0300

    feat: create keyboard dinamicly

--- hangman-game/js/main.js
+++ hangman-game/js/main.js
@@ -1,5 +1,6 @@
 const task = document.querySelector(".game-task");
 const wordDisplay = document.querySelector(".word");
+const keyboardWrapper = document.querySelector(".keyboard");
 
 // выбор случайного слова из списка
 const getRandomWord = () => {
@@ -9,6 +10,18 @@ const getRandomWord = () => {
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
+const initGame = (button, clickedLetter) => {
+    console.log(button, clickedLetter);
+}
+
 // создание клавиатуры
+for (let i = 1040; i <= 1071; i++) {
+    const letterBtn = document.createElement("button");
+    letterBtn.innerText = String.fromCharCode(i);
+    letterBtn.classList.add("btn-letter");
+    keyboardWrapper.appendChild(letterBtn);
+    letterBtn.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+
+}
 
 getRandomWord();
