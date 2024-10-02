f617dc1a91a2358a22ee8289c395981ebabd6ba5
Julia Grigoreva <julia_panina@mail.ru>
2024-10-02 16:33:20 +0300

    feat: show correct words, count incorrect words

--- hangman-game/js/main.js
+++ hangman-game/js/main.js
@@ -1,21 +1,37 @@
 const task = document.querySelector(".game-task");
 const wordDisplay = document.querySelector(".word");
 const keyboardWrapper = document.querySelector(".keyboard");
+const guessesText = document.querySelector(".game-score");
+let currentWord, wrongGuessCount = 0;
+const maxGuesesCount = 6;
 
 // выбор случайного слова из списка
 const getRandomWord = () => {
-    const {word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(word, hint);
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word;
+    console.log(word);
     task.innerText = hint;
     wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
 const initGame = (button, clickedLetter) => {
-    console.log(button, clickedLetter);
+    // проверка есть ли буква в слове
+    if(currentWord.includes(clickedLetter)) {
+        // показать верные буквы в слове
+        [...currentWord].forEach((letter, index) => {
+            if(letter == clickedLetter) {
+                wordDisplay.querySelectorAll(".letter")[index].innerText = letter;
+                wordDisplay.querySelectorAll(".letter")[index].classList.add("guessed");
+            }
+        })
+    } else {
+        wrongGuessCount++;
+    }
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesesCount}`;
 }
 
 // создание клавиатуры
-for (let i = 1040; i <= 1071; i++) {
+for (let i = 1072; i <= 1103; i++) {
     const letterBtn = document.createElement("button");
     letterBtn.innerText = String.fromCharCode(i);
     letterBtn.classList.add("btn-letter");
